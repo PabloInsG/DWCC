4513e390a22572ac986bb2c780288679fb42cc51
Pablo <pablo.insua.gerpe@gmail.com>
2023-09-12 14:22:21 +0200

    Day 1

--- js.js
+++ js.js
@@ -1,1 +1,18 @@
-console.log("Hola");
+let num1 = 2;
+let num2 = 3;
+
+let suma = num1 + num2;
+
+console.log(suma, num1);
+//document.write(suma, num2);
+
+let nombre = prompt( "Introduce un nombre");
+let apellido = prompt( "Introduce tu apellido");
+let edad = prompt( "Introduce tu edad");
+
+document.write("<h1>", "Datos Recollidos:", "</h1>")
+
+document.write(`<strong>Nombre: \"${nombre}" <br> Apellido:  ${apellido} <br> Edad: ${edad} </strong>`);
+
+
+
