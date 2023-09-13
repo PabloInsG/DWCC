55ad1415e77a8f58c2a393130d460f655c037592
Pablo <pablo.insua.gerpe@gmail.com>
2023-09-13 16:38:41 +0200

    Objeto Number

--- js.js
+++ js.js
@@ -12,7 +12,21 @@ let edad = prompt( "Introduce tu edad");
 
 document.write("<h1>", "Datos Recollidos:", "</h1>")
 
-document.write(`<strong>Nombre: \"${nombre}" <br> Apellido:  ${apellido} <br> Edad: ${edad} </strong>`);
+document.write(`<strong>Nombre: \"${nombre}" <br> Apellido:  \"${apellido}" <br> Edad: \"${edad}" <br></strong>`);
+
+document.write("C:\\\\usuario\\ tarda 1\'23\", algo considerado \"lento\" en la actualidad.")
 
 
+var numeroBinario = 0b1111;
+var numeroHexadecimal = 0x2ba;
+var numeroOctal = 0o111;
+var potencia= 2e+2;
 
+var divisionPorCero = 23/0;
+var sumaMasDivision = divisionPorCero + 12;
+
+console.log(numeroBinario);
+console.log(numeroHexadecimal);
+console.log(numeroOctal);
+console.log(potencia);
+console.log(sumaMasDivision);
